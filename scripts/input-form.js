90659c326091b6967d83fa7db6ed16ed24161835
NelliYanchuk <yanchuk.nelli@gmail.com>
2024-10-20 23:27:50 +0200

    add three functions

--- scripts/input-form.js
+++ scripts/input-form.js
@@ -1,14 +1,41 @@
-const inputTXT = document.getElementById('tb-string');
-const functionUSE = document.getElementById('func');
-const enterBTN = document.getElementById('enter-btn');
-const outputTXT = document.getElementById('output-box');
+const inputTXT = document.getElementById('tb-string')
+const functionUSE = document.getElementById('func')
+const enterBTN = document.getElementById('enter-btn')
+const outputTXT = document.getElementById('output-box')
+const vowels = 'aeiouAEIOU';
 
-function getSentence() {
-    outputTXT.innerHTML = inputTXT.value;
+function getSentence () {
+  outputTXT.innerHTML = inputTXT.value
 }
 
-enterBTN.addEventListener('click', function() {
-    if (functionUSE.value === 'getSentence') {
-        getSentence();
-    }
-});
+function getVocals () {
+  let vocal = '';
+  var str = inputTXT.value;
+  for (let char of str) {
+    if (vowels.includes(char) && !vocal.includes(char)) {
+        vocal+=char;
+    }}
+    outputTXT.innerHTML = vocal || 'No vowels in your string';
+}
+
+function searchVocals () {
+    let vocal = [];
+    var str = inputTXT.value;
+    for (let char of str) {
+      if (vowels.includes(char) && !vocal.includes(char)) {
+        vocal.push(char);
+      }}
+      outputTXT.innerHTML = vocal || 'No vowels in your string';
+  }
+
+enterBTN.addEventListener('click', function () {
+  if (functionUSE.value === 'getSentence') {
+    getSentence()
+  } else if (functionUSE.value === 'searchVocals') {
+    searchVocals()
+  } else if (functionUSE.value === 'deleteDuplicity') {
+    deleteDuplicity()
+  } else if (functionUSE.value === 'getVocals') {
+    getVocals()
+  }
+})
